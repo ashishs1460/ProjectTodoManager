6ae0478cdf433f7cc1b90044f66775172d65ad83
Ashish S <ashish@Ashishs-MacBook-Air.local>
2024-08-07 20:15:52 +0530

    create backend structure using spring boot

---
+++ src/test/java/com/ashish/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
